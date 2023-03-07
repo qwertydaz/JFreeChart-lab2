678dc4a37683dbafbdeb7dc228e17411e61a8ab8
Dáire McDermott <dairemcdermott@outlook.com>
2023-03-07 21:28:40 +0000

    initial commit

---
+++ src/org/jfree/data/test/RangeTest.java
@@ -0,0 +1,20 @@
+package org.jfree.data.test;
+
+import junit.framework.TestCase;
+import org.jfree.data.Range;
+import org.junit.*;
+
+public class RangeTest
+{
+	private Range rangeObjectUnderTest;
+
+	@Before
+	public void setUp() throws Exception
+	{
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+	}
+}
