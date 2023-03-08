91b54678aa5d174b70ded95651397a0bc4f072ca
Dáire McDermott <dairemcdermott@outlook.com>
2023-03-08 20:14:44 +0000

    added new tests from test plan

--- src/org/jfree/data/test/RangeTest.java
+++ src/org/jfree/data/test/RangeTest.java
@@ -1,6 +1,8 @@
 package org.jfree.data.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.jfree.data.Range;
 
@@ -11,17 +13,21 @@ import org.junit.Test;
 public class RangeTest
 {
 	private Range rangeObjectUnderTest;
+	private Range rangeObjectUnderTest2;
 
 	@Before
 	public void setUp() throws Exception
 	{
 		rangeObjectUnderTest = new Range(-1, 1);
+		rangeObjectUnderTest2 = new Range(4, 9);
 	}
 
 	@After
 	public void tearDown() throws Exception
 	{
 	}
+	
+	// EXAMPLE TESTS
 	
 	@Test
 	public void testCentralValueShouldBeZero()
@@ -80,5 +86,150 @@ public class RangeTest
 				8.0,
 				r5.getLength(),
 				0.000000001d);
+	}
+	
+	// ADDED TESTS
+	
+	// Method Under Test: intersects
+	
+	@Test
+	public void testIntersectsLowerAndUpperIsSameAsObject()
+	{
+		assertEquals("intersects: Did not return the expected output",
+				true,
+				rangeObjectUnderTest2.intersects(4, 9));
+	}
+	
+	@Test
+	public void testIntersectsLowerIsJustBelowLowerBound()
+	{
+		assertEquals("intersects: Did not return the expected output",
+				true,
+				rangeObjectUnderTest2.intersects(3, 4));
+	}
+	
+	@Test
+	public void testIntersectsUpperIsJustAboveUpperBound()
+	{
+		assertEquals("intersects: Did not return the expected output",
+				true,
+				rangeObjectUnderTest2.intersects(9, 10));
+	}
+	
+	@Test
+	public void testIntersectsSmallPossibleRange()
+	{
+		assertEquals("intersects: Did not return the expected output",
+				false,
+				rangeObjectUnderTest2.intersects(0, 0));
+	}
+	
+	@Test
+	public void testIntersectsLargestPossibleRange()
+	{
+		assertEquals("intersects: Did not return the expected output",
+				true,
+				rangeObjectUnderTest2.intersects(Double.MIN_VALUE,
+						Double.MAX_VALUE));
+	}
+	
+	// Method Under Test: constrain
+	
+	@Test
+	public void testConstrainValueEqualToLowerBound()
+	{
+		assertEquals("contrain: Did not return the expected output",
+				4,
+				rangeObjectUnderTest2.constrain(4),
+				0.000000001d);
+	}
+	
+	@Test
+	public void testConstrainValueEqualToUpperBound()
+	{
+		assertEquals("contrain: Did not return the expected output",
+				9,
+				rangeObjectUnderTest2.constrain(9),
+				0.000000001d);
+	}
+	
+	@Test
+	public void testConstrainValueJustBelowLowerBound()
+	{
+		assertEquals("contrain: Did not return the expected output",
+				4,
+				rangeObjectUnderTest2.constrain(3),
+				0.000000001d);
+	}
+	
+	@Test
+	public void testConstrainValueJustAboveUpperBound()
+	{
+		assertEquals("contrain: Did not return the expected output",
+				9,
+				rangeObjectUnderTest2.constrain(10),
+				0.000000001d);
+	}
+	
+	@Test
+	public void testConstrainValueFarBelowLowerBound()
+	{
+		assertEquals("contrain: Did not return the expected output",
+				4,
+				rangeObjectUnderTest2.constrain(-99),
+				0.000000001d);
+	}
+	
+	@Test
+	public void testConstrainValueFarAboveUpperBound()
+	{
+		assertEquals("contrain: Did not return the expected output",
+				9,
+				rangeObjectUnderTest2.constrain(99),
+				0.000000001d);
+	}
+	
+	// Method Under Test: contains
+	
+	@Test
+	public void testContainsValueEqualToLowerBound()
+	{
+		assertTrue("contains: Did not return the expected output",
+				rangeObjectUnderTest2.contains(4));
+	}
+	
+	@Test
+	public void testContainsValueEqualToUpperBound()
+	{
+		assertTrue("contains: Did not return the expected output",
+				rangeObjectUnderTest2.contains(9));
+	}
+	
+	@Test
+	public void testContainsValueJustBelowLowerBound()
+	{
+		assertFalse("contains: Did not return the expected output",
+				rangeObjectUnderTest2.contains(3));
+	}
+	
+	@Test
+	public void testContainsValueJustAboveUpperBound()
+	{
+		assertFalse("contains: Did not return the expected output",
+				rangeObjectUnderTest2.contains(10));
+	}
+	
+	@Test
+	public void testContainsValueFarBelowLowerBound()
+	{
+		assertFalse("contains: Did not return the expected output",
+				rangeObjectUnderTest2.contains(-99));
+	}
+	
+	@Test
+	public void testContainsValueFarAboveUpperBound()
+	{
+		assertFalse("contains: Did not return the expected output",
+				rangeObjectUnderTest2.contains(99));
 	}
 }
