363eb1da1fa7bbecb124869a67e5e7cf25d7c7cd
Dáire McDermott <dairemcdermott@outlook.com>
2023-03-09 00:08:30 +0000

    adding DataUtilitiesTest code

--- src/org/jfree/data/test/DataUtilitiesTest.java
+++ src/org/jfree/data/test/DataUtilitiesTest.java
@@ -1,25 +1,33 @@
 package org.jfree.data.test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import org.jfree.data.DataUtilities;
 import org.jfree.data.DefaultKeyedValues;
 import org.jfree.data.DefaultKeyedValues2D;
 import org.jfree.data.KeyedValues;
 import org.jfree.data.Values2D;
-
+import junit.framework.TestCase;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class DataUtilitiesTest
+public class DataUtilitiesTest extends DataUtilities 
 {
 	private Values2D values2D;
 
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
 	@Before
-	public void setUp() throws Exception
+	public void setUp() throws Exception 
 	{
 		DefaultKeyedValues2D testValues = new DefaultKeyedValues2D();
 		values2D = testValues;
@@ -28,48 +36,110 @@ public class DataUtilitiesTest
 	}
 
 	@After
-	public void tearDown() throws Exception
+	public void tearDown() throws Exception 
 	{
 		values2D = null;
 	}
-	
+
 	@Test
-	public void testValidDataAndColumnColumnTotal()
+	public void testValidDataAndColumnColumnTotal() 
 	{
 		assertEquals("Wrong sum returned. It should be 5.0",
-				5.0,
-				DataUtilities.calculateColumnTotal(values2D, 0),
-				0.000000001d); 
+						5.0, DataUtilities.calculateColumnTotal(values2D,  0), 0.0000001d);
 	}
 	
-	@Test
-	public void testGetCumulativePercentages()
+	@Test // getCumlativePrecentages Function
+	public void testgetCumlativePercenetages() 
 	{
-		DefaultKeyedValues keyValues = new DefaultKeyedValues();
-		keyValues.addValue((Comparable) 0.0, 6.0);
-		keyValues.addValue((Comparable) 1.0, 11.0);
-		keyValues.addValue((Comparable) 2.0, 3.0);
-		KeyedValues objectUnderTest =
-				DataUtilities.getCumulativePercentages((KeyedValues) keyValues);
+		// setup
 		
-		assertEquals((double) objectUnderTest.getValue(2),
-				1.0,
-				0.000000001d);
+		DefaultKeyedValues keyvalues = new DefaultKeyedValues();
+		keyvalues.addValue((Comparable) 0.0, 6.0);
+		keyvalues.addValue((Comparable) 1.0, 11.0);
+		keyvalues.addValue((Comparable) 2.0, 3.0);
+		KeyedValues object_under_test = DataUtilities.getCumulativePercentages((KeyedValues) keyvalues);
+		
+		assertEquals((double) object_under_test.getValue(2), 1.0, .000000001d);
 	}
 	
-	@Test
+	@Test // calculateColumnTotal function
 	public void testNullDataColumnTotal()
 	{
 		try
 		{
-			DataUtilities.calculateColumnTotal(null, 0);
-			fail("No exception thrown. The expected outcome was: a thrown"
-					+ "exception of type: IllegalArgumentException");
+			DataUtilities.calculateColumnTotal(null,  0);
+			fail("No exception thrown. The expected outcome was: a thrown exception of type: IllegalArgumentException");
+					 			
 		}
 		catch (Exception e)
 		{
-			assertTrue("Incorrect exception type thrown",
+			assertTrue("Incorrest exception type thrown",
 					e.getClass().equals(IllegalArgumentException.class));
 		}
 	}
+	
+	@Test // calculateRowTotal
+	public void testNullDataRowTotal()
+	{
+		try
+		{
+			DataUtilities.calculateRowTotal(null,  0);
+			fail("No exception thrown. The expected outcome was: a thrown exception of type: IllegalArgumentException");
+					 			
+		}
+		catch (Exception e)
+		{
+			assertTrue("Incorrest exception type thrown",
+					e.getClass().equals(IllegalArgumentException.class));
+		}
+	}
+	
+	@Test // createNumberArray 
+	public void testNullcreateNumberArray()
+	{
+		try
+		{
+			DataUtilities.createNumberArray(null);
+			fail("No exception thrown. The expected outcome was: a thrown exception of type: IllegalArgumentException");
+					 			
+		}
+		catch (Exception e)
+		{
+			assertTrue("Incorrest exception type thrown",
+					e.getClass().equals(IllegalArgumentException.class));
+		}
+	}
+	
+	@Test // createNumberArray2D 
+	public void testNullcreateNumberArray2D()
+	{
+		try
+		{
+			DataUtilities.createNumberArray2D(null);
+			fail("No exception thrown. The expected outcome was: a thrown exception of type: IllegalArgumentException");
+					 			
+		}
+		catch (Exception e)
+		{
+			assertTrue("Incorrest exception type thrown",
+					e.getClass().equals(IllegalArgumentException.class));
+		}
+	}
+	
+	@Test // getCumulativePercentages 
+	public void testNullgetCumulativePercentages()
+	{
+		try
+		{
+			DataUtilities.getCumulativePercentages(null);
+			fail("No exception thrown. The expected outcome was: a thrown exception of type: IllegalArgumentException");
+					 			
+		}
+		catch (Exception e)
+		{
+			assertTrue("Incorrest exception type thrown",
+					e.getClass().equals(IllegalArgumentException.class));
+		}
+	}
+
 }
